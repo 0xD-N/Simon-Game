b1538db63a90a2cd9f42fdd02b3a0432e86628e7
0xD-N <63315159+0xD-N@users.noreply.github.com>
2022-01-25 21:52:16 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 let show_instructions = false;
 let started = false;
 let gameTextInterval = null;
+let levelInterval = null;
+let run = false;
+let computer_sequence = [];
+let user_sequence = [];
+let i = 0;
+let level = 1;
+
+let animals = ["bird", "dog", "lion", "monkey"]
+
 
 function getRandomColor() 
 {
@@ -19,14 +28,6 @@ function rainbowGameText()
     $("#game-text").css("color", getRandomColor())
 }
 
-
-
-$(document).ready(function() 
-{
-    gameTextInterval = setInterval(rainbowGameText, 1000)
-})
-
-
 let instructions = function()
 {
     if(show_instructions)
@@ -40,4 +41,103 @@ let instructions = function()
         show_instructions = true
     }
 }
+
+let checkSequence = function()
+{
+    let equal = true
+    for(let i = 0; i < user_sequence.length; i++)
+    {
+        if((user_sequence[i] == computer_sequence[i]) == false)
+            equal = false
+    }
+    return equal
+}
+
+let addSequence = function()
+{
+    computer_sequence.push(Number(1 + Math.random() * 3).toFixed(0))
+}
+
+let playSequence = function()
+{
+    for(let i = 0; i < computer_sequence.length; i++)
+    {
+        alert(Number(computer_sequence[i]).toFixed(0))
+    }
+}
+
+let begin = function()
+{
+    $("#start").hide()
+
+    $("#game-text").text(`Level ${level}`)
+    
+    run=true
+
+    addSequence()
+
+    playSequence()
+}
+
+let logic = function(num)
+{
+    if(run)
+    {
+        user_sequence.push(num)
+
+        if(!checkSequence())
+        {
+            $("#game-text").text("GAME OVER!")
+            clearInterval(gameTextInterval)
+            run = false;
+        }
+        else
+        {
+            if(user_sequence.length == computer_sequence.length)
+            {
+                user_sequence = []
+                addSequence()
+                level += 1
+                playSequence()
+            }
+        }
+    }
+    
+}
+
+let setLevel = function()
+{
+    if(run)
+    {
+        $("#game-text").text(`Level ${level}`)
+    }
+}
+
+$(document).ready(function() 
+{
+    gameTextInterval = setInterval(rainbowGameText, 1000)
+    levelInterval = setInterval(setLevel, 1000)
+    $("#start").click(function() 
+    {
+        begin()
+    })
+    
+    $("#bird").click(function()
+    {
+        logic(1)
+    });
+    $("#dog").click(function()
+    {
+        logic(2);
+    });
+    $("#lion").click(function()
+    {
+        logic(3);
+    });
+    $("#monkey").click(function()
+    {
+        logic(4)
+    })
+})
+
 
